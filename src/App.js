8eb68614b87744b617d78ccb00cc991b44b2a504
Andrey <andreyshamray86@gmail.com>
2021-03-01 14:20:54 +0300

    further basics practice

--- src/App.js
+++ src/App.js
@@ -1,70 +1,28 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
+
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
 class App extends Component {
   state = {
-    persons: [
-      {name: "Andy", age: 34},
-      {name: "Max", age: 36},
-      {name: "Olya", age: 35}
-    ]
+    username: 'supermax'
   }
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        {name: newName, age: 34},
-        {name: "Max", age: 37},
-        {name: "Olya", age: 35}
-      ]
-    });
+  usernameChangedHandler = (event) => {
+    this.setState({username: event.target.value});
   }
-
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        {name: "Andy", age: 34},
-        {name: event.target.value, age: 37},
-        {name: "Olya", age: 35}
-      ]
-    });
-  }
-
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
-    return ( 
-    <div className = "App" >
-      <h1> First Steps </h1> 
-      
-      <button
-      style={style} 
-      onClick={() => this.switchNameHandler('Andrey')}
-      >Switch Name</button>
-      
-      <Person 
-      name={this.state.persons[0].name} 
-      age={this.state.persons[0].age}/>  
-      
-      <Person 
-      name={this.state.persons[1].name} 
-      age={this.state.persons[1].age}
-      click={this.switchNameHandler.bind(this, 'Andrushka')}
-      changed={this.nameChangeHandler}>My hobbies: Press
-      </Person>
-
-      <Person 
-      name={this.state.persons[2].name} 
-      age={this.state.persons[2].age}/>
-    </div>
+    return (
+      <div className="App">
+        <UserInput 
+          changed={this.usernameChangedHandler} 
+          currentName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName="Max" />
+      </div>
     );
   }
 }

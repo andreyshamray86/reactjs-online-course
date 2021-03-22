2ee28b1fa034ad3514f724de284ca3c1087f5734
Andrey <andreyshamray86@gmail.com>
2021-03-22 14:38:02 +0300

    added authentication

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -30,7 +30,7 @@ export const fetchIngredientsFailed = () => {
 
 export const initIngredients = () => {
     return dispatch => {
-        axios.get('https://react-myburger-c3af3-default-rtdb.firebaseio.com/ingredients.json')
+        axios.get( 'https://react-myburger-c3af3-default-rtdb.firebaseio.com/ingredients.json' )
             .then( response => {
                dispatch(setIngredients(response.data));
             } )
